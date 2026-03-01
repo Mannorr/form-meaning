2849f48dd42959efb0c3ea6e79aef2789358a8fc
agoluwanifemi-cloud <ag.oluwanifemi@gmail.com>
2026-03-01 12:50:11 +0000

    Force dynamic pages

--- app/library/page.js
+++ app/library/page.js
@@ -1,6 +1,8 @@
 import { supabaseAdmin } from "@/lib/supabaseAdmin";
 import LibraryPage from "../components/library-page";
 
+export const dynamic = "force-dynamic";
+
 export default async function Library() {
   const { data: content } = await supabaseAdmin.from("content").select("*").eq("status", "published").order("created_at", { ascending: false });
   return <LibraryPage content={content || []} />;
